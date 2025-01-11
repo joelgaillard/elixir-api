940b4949561c4ae488128aa4892f489fc82bb27e
Joël Gaillard <joelgaillard@joelgaillard-4.local>
2025-01-11 17:54:54 +0100

    Config CORS

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
 
 import usersRouter from "./routes/users.js";
-import cocktailsRouter from './routes/cocktails.js';
-import ingredientsRouter from './routes/ingredients.js';
-import barsRouter from './routes/bars.js';
+import cocktailsRouter from "./routes/cocktails.js";
+import ingredientsRouter from "./routes/ingredients.js";
+import barsRouter from "./routes/bars.js";
 
 dotenv.config();
 
@@ -20,9 +21,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.DATABASE_URL)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+// Configuration CORS
+app.use(
+  cors({
+    // Configuration CORS
+    origin: "https://elixir-icgi.onrender.com",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware
 app.use(logger("dev"));
@@ -30,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Configure apiDoc to serve documentation
-app.use('/', express.static(path.join(__dirname, 'docs')));
+app.use("/", express.static(path.join(__dirname, "docs")));
 
 // API routes
 app.use("/api/users", usersRouter);
@@ -48,9 +60,12 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  
+
   res.status(err.status || 500);
   res.json({ error: res.locals.message });
 });
+
+// Enable CORS
+app.use(cors());
 
 export default app;
